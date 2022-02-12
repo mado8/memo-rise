26446a5247722189547f8a448bb02c59fa932d4e
Carson <carsoncolgateccu@gmail.com>
2022-02-12 12:38:45 -0700

    solving login bugs

--- memorize/client/src/components/LoginSignup.js/Login.jsx
+++ memorize/client/src/components/LoginSignup.js/Login.jsx
@@ -1,98 +1,73 @@
 import React, { useState } from 'react'
 const AuthService = require('../../utils/auth')
+import { loginUser } from '../../utils/mutation';
+import Auth from '../../utils/auth';
 
+const LoginForm = () => {
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        try {
+            const response = await loginUser(userFormData);
+
+            if (!response.ok) {
+                throw new Error('something went wrong!');
+            }
+
+            const { token, user } = await response.json();
+            console.log(user);
+            Auth.login(token);
+        } catch (err) {
+            console.error(err);
+            setShowAlert(true);
+        }
+
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
+    };
+}
 const LoginFormComponent = () => {
 
-  const [userFormData, setUserFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  })
-  
-  const [errors, setErrors] = useState({})
-
-  const handleChange = (e) => {
-    setUserFormData[e.target.name] = e.target.value
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (validate()) {
-
-      setUserFormData['name'] = ''
-      setUserFormData['email'] = ''
-      setUserFormData['message'] = ''
-
-      //re-route to homepage from here if successfully logged in
-    }
-  }
-
-  const validate = () => {
-
-    setErrors = {}
-    let isValid = true
-
-    if (!userFormData['name']) {
-      isValid = false
-      errors['name'] = 'Please enter your name.'
-    }
-
-    if (!userFormData['email']) {
-      isValid = false
-      errors['email'] = 'Please enter your email Address.'
-    }
-
-    if (typeof userFormData['email'] !== 'undefined') {
-      var pattern = new RegExp(
-        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
-      )
-      if (!pattern.test(userFormData['email'])) {
-        isValid = false
-        errors['email'] = 'Please enter valid email address.'
-      }
-    }
-
-    if (!userFormData['message']) {
-      isValid = false
-      errors['message'] = 'Please enter your message.'
-    }
-
-    if (errors.name) {
-      alert(errors.name)
-    }
-    if (errors.email) {
-      alert(errors.email)
-    }
-    if (errors.message) {
-      alert(errors.message)
-    }
-
-    // add auth
-    
-    return isValid
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit()}>
+    return (
         <div>
-          <label>
-            <input name='username' type='text' placeholder='Username' onChange={handleChange()}/>
-          </label>
+            <form>
+                <div>
+                    <label>
+                        <input name='username' type='text' placeholder='Username' />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input name='email' type='text' placeholder='Email' />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input name='password' type='text' placeholder='Password' />
+                    </label>
+                </div>
+                <button type='submit'> Submit</button>
+            </form>
         </div>
-        <div>
-          <label>
-            <input name='email' type='text' placeholder='Email' onChange={handleChange()}/>
-          </label>
-        </div>
-        <div>
-          <label>
-            <input name='password' type='text' placeholder='Password' onChange={handleChange()}/>
-          </label>
-        </div>
-      </form>
-    </div>
-  )
+    )
 }
-
 export default LoginFormComponent
