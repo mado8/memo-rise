4b551ec682b40f751920454066d06f27eab29e94
Hiro Protagonist <shdesai118@gmail.com>
2022-02-16 14:53:12 -0700

    added bcrpy and login resolver

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const memorySchema = require('./Memory')
+// const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -22,7 +23,19 @@ const userSchema = new Schema({
     },
     memory: [memorySchema]
 });
-
+// userSchema.pre('save', async function (next) {
+//     if (this.isNew || this.isModified('password')) {
+//       const saltRounds = 10;
+//       this.password = await bcrypt.hash(this.password, saltRounds);
+//     }
+  
+//     next();
+//   });
+  
+//   // custom method to compare and validate password for logging in
+//   userSchema.methods.isCorrectPassword = async function (password) {
+//     return bcrypt.compare(password, this.password);
+//   };
 const User = model('User', userSchema);
 
 module.exports = User;
