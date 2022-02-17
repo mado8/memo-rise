804d83b7e13eb97be5d0dc2129ae61405be8735d
unknown <mccullochryan636@gmail.com>
2022-02-17 13:49:44 -0700

    started create memory component

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './Home/home'
+import createMemory from './CreateMemory/CreateMemory';
 
 // import components into main container and conditionally render them.
 
