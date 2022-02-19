6a3a9e6a5af1a6e54f8678a990751704e44abc40
mdavie <marianahodniki1991@gmail.com>
2022-02-19 11:02:53 -0500

    css done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Container from "./components/MainContainer"
-import DailyActivites from './components/DailyActivities/DailyActivites.jsx'
-import CreateMemory from './components/CreateMemory/CreateMemory'
+// import Container from "./components/MainContainer"
+// import DailyActivites from './components/DailyActivities/DailyActivites.jsx'
+// import CreateMemory from './components/CreateMemory/CreateMemory'
 import DashboardComponent from './components/Dashboard/Dashboard'
 
 function App() {
   return (
     <div>
-      <CreateMemory />
+      {/* <CreateMemory /> */}
+      <DashboardComponent/>
     </div>
   )
 }
