f0f8e8944d956cd97474a015fbed5f60e13b69da
mado <madelineadonley@gmail.com>
2022-02-25 11:31:10 -0700

    article page debugging

--- client/src/components/Article/Article.jsx
+++ client/src/components/Article/Article.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ArticlesPage = () => {
-    var url = 'https://newsapi.org/v2/?' +
-          'q=memory&' +
-          'category=science' +
-          'sortBy=popularity&' +
-          'apiKey=215f9f133cb64c8f896fbbf3ddc0e96d';
 
-    var req = new Request(url);
+    // const newsapi = new NewsAPI('215f9f133cb64c8f896fbbf3ddc0e96d')
+    // const [data, setData] = useState([]);
 
-    fetch(req)
-        .then(function(response) {
-        console.log(response.json());
-    })
+    useEffect(() => {
+        let url = 'https://memo-rise.herokuapp.com/https://newsapi.org/v2/?' +
+        'q=memory&' +
+        'category=science' +
+        'sortBy=popularity&' +
+        'apiKey=5326392fc10344bb8a8fe41dd80aa651';
+
+        var req = new Request(url);
+
+        fetch(req, {
+            mode: 'no-cors'
+        })
+            .then(function(response) {
+            console.log(response)
+        })
+    },[])
 
     return (
         <div>Articles Page!</div>
