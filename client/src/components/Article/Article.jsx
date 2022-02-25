29b55cfb3a3516c456e7fe10f75f6af3140750a2
mado <madelineadonley@gmail.com>
2022-02-25 11:59:18 -0700

    got api to work

--- client/src/components/Article/Article.jsx
+++ client/src/components/Article/Article.jsx
@@ -1,30 +1,25 @@
 import React, { useEffect, useState } from 'react'
 
 const ArticlesPage = () => {
+  // const newsapi = new NewsAPI('215f9f133cb64c8f896fbbf3ddc0e96d')
+  // const [data, setData] = useState([]);
 
-    // const newsapi = new NewsAPI('215f9f133cb64c8f896fbbf3ddc0e96d')
-    // const [data, setData] = useState([]);
+  useEffect(() => {
+    var url =
+      'https://newsapi.org/v2/everything?' +
+      'q=Memorization&' +
+      'searchIn=title&' +
+      'sortBy=popularity&' +
+      'apiKey=5326392fc10344bb8a8fe41dd80aa651'
 
-    useEffect(() => {
-        let url = 'https://memo-rise.herokuapp.com/https://newsapi.org/v2/?' +
-        'q=memory&' +
-        'category=science' +
-        'sortBy=popularity&' +
-        'apiKey=5326392fc10344bb8a8fe41dd80aa651';
+    var req = new Request(url)
 
-        var req = new Request(url);
+    fetch(req).then(function (response) {
+      console.log(response.json())
+    })
+  }, [])
 
-        fetch(req, {
-            mode: 'no-cors'
-        })
-            .then(function(response) {
-            console.log(response)
-        })
-    },[])
-
-    return (
-        <div>Articles Page!</div>
-    )
+  return <div>Articles Page!</div>
 }
 
 export default ArticlesPage
