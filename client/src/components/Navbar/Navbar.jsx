07b675dabd86f655e668ccc07938e4d52ab74944
mdavie <marianahodniki1991@gmail.com>
2022-02-25 22:29:44 -0700

    navbar kill session

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import './navbar.css'
 import Auth from '../../utils/auth';
+import { useState } from 'react'
 // import LoginForm from '../LoginSignup.js/Login'
 // import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [setShowModal] = useState(false);
   return (
-    <div>
-      <ul id="nav-list">
-        {/* <a className="nav-link" href="#">Features</a> */}
+    
+    <div>      
+    <ul id="nav-list">
+      {/* <a className="nav-link" href="#">Features</a> */}
+    {Auth.loggedIn() ? ( 
+      <>
         <li>
           <a href="/dashboard">Home</a>
         </li>
+          
         <li>
           <a href="/" onClick={Auth.logout}>Logout</a>
         </li>
-      </ul>
-    </div>
+      </>
+      ):(
+        <li>
+          <a href="/login" onClick={() => setShowModal(true)}>Login/Sign Up</a>
+        </li>      
+    )}
+    
+    </ul>
+  </div>
   )
 }
