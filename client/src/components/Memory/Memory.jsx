0179c001d65ca4923b8131562cbfbee5153c884d
mado <madelineadonley@gmail.com>
2022-02-25 18:10:36 -0700

    added stuff

--- client/src/components/Memory/Memory.jsx
+++ client/src/components/Memory/Memory.jsx
@@ -5,25 +5,28 @@ import Question from '../../Questions/AddQuestion'
 import AddQuestion from '../../Questions/AddQuestion'
 
 
-const useMemory = ({ _id }) => {
-  console.log(_id)
+const useMemory = ({ _id, handlePage }) => {
+  // console.log(_id)
   const { data: memory } = useQuery(GET_MEMORY, {
     variables: { _id },
   })
   const memoryTitle = memory?.memory.title
   const memoryDesc = memory?.memory.description
+  const memoryQuestionsArr = memory?.memory.questions
+  console.log(memoryQuestionsArr);
+
   const memoryQuestions = memory?.memory.questions || [{
     _id : "one two three",
     question: "this a question?",
     answer: "answer"
   }]
   // const memoryTitle = memory.title;
-  console.log(memoryQuestions)
+  // console.log(memoryQuestions)
 
-  if (memory !== null) {
-    console.log(memoryTitle)
-    console.log(memoryDesc)
-  }
+  // if (memory !== null) {
+  //   console.log(memoryTitle)
+  //   console.log(memoryDesc)
+  // }
 
   return (
     <div className="memory-div">
@@ -31,7 +34,7 @@ const useMemory = ({ _id }) => {
       <p className="memory-item">{memoryTitle}</p>
       <h3> Description </h3>
       <p className="memory-item"> {memoryDesc}</p>
-      {/* < AddQuestion _id={memory._id} /> */}
+      <a id="questions-button" questionsID={1} onClick={()=> handlePage('questions')}>See questions!</a>
     </div>
   )
 }
--- client/src/components/myMemories/MyMemories.jsx
+++ client/src/components/myMemories/MyMemories.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_ME, GET_MEMORY } from '../../utils/querie'
 import Memory from '../Memory/Memory'
@@ -6,35 +6,47 @@ import './MyMemories.css'
 // need to grab user's memories and render each as a div with a image and a button
 
 const MyMemories = () => {
-    const { data: user } = useQuery(GET_ME);
-    const userData = user?.user || {};
-    let arr = [];
+  const [page, setPage] = useState('memories')
+  const { data: user } = useQuery(GET_ME)
+  const userData = user?.user || {}
+  let arr = []
 
-    const returnMemories = () => {
-        const memoryIdArr = userData.memories;
-        if (memoryIdArr !== undefined) {
-            console.log(memoryIdArr[0]._id)
+  const handlePage = (pageSet) => {
+    setPage(pageSet);
+  }
 
-            const memoryItems = memoryIdArr.map((memory) => {
-                return (
-                    <Memory _id={memory._id}></Memory>
-                )
-            })
-            memoryItems.forEach(memory => {
-                arr.push(memory)
-            })
-
-            return arr;
-        }
-    }
-
-    return (
-        <div id="memory-component">
-            <div className="memory-container">
-                {returnMemories()}
-            </div>
+  const returnPage = () => {
+    if (page === 'questions') {
+      return (
+          <div id="coming-soon">Coming Soon!  ♡ ◡ ♡</div>
+      )
+    } else {
+      return (
+        <div id='memory-component'>
+          <div className='memory-container'>{returnMemories()}</div>
         </div>
-    )
+      )
+    }
+  }
+
+  const returnMemories = () => {
+    const memoryIdArr = userData.memories
+    if (memoryIdArr !== undefined) {
+      console.log(memoryIdArr[0]._id)
+
+      const memoryItems = memoryIdArr.map((memory) => {
+        return <Memory _id={memory._id} handlePage={handlePage}></Memory>
+      })
+
+      memoryItems.forEach((memory) => {
+        arr.push(memory)
+      })
+
+      return arr
+    }
+  }
+
+  return returnPage();
 }
 
-export default MyMemories;
+export default MyMemories
